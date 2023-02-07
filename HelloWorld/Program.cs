65262091a9d774a8090ee3c4666097ac0699e934
Stasiu <Stasiu@STANLEY>
2023-02-07 11:05:26 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Stanisław Wolny");
+            Console.WriteLine("Siemka");
+            Console.WriteLine("Do następnej lekcji");
+        }
+    }
+}
+
